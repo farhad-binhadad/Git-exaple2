534f2900c1919ce05dbd75eb8ba7880c7049f114
tonijechukwudi <tonijechukwudi@gmail.com>
2020-06-07 03:49:04 +0100

    lst commit

--- loops.py
+++ loops.py
@@ -4,20 +4,3 @@ print ("My name is Chukwudi Theodore Onije")
 
 line for new branch_new
 
-
-<<<<<<< HEAD
-added branch new2 for try:
-    pass
-except expression as identifier:
-    pass
-else:
-    pass
-
-import netmiko
-    async for target in iter:
-        block
-    else:
-        block
-=======
-another one 
->>>>>>> new
