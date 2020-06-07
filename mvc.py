00e3fd3bd4171f7ab21847663e0c3ac8240779de
tonijechukwudi <tonijechukwudi@gmail.com>
2020-06-07 01:53:00 +0100

    intital Commit

---
+++ mvc.py
@@ -0,0 +1,54 @@
+class Device:
+    
+    ipAddress = ""
+    port = ''
+    
+    @staticmethod
+    def findDevices():
+        devices = []
+        
+        d = Device()
+        d.ipAddress = "192.168.1.1"
+        d.port = "2001"
+        
+        devices.append(d)
+        
+        d = Device()
+        d.ipAddress = "192.168.1.50"
+        d.port = "7001"
+        
+        devices.append(d)
+        
+        d = Device()
+        d.ipAddress = "192.168.1.100"
+        d.port = "80"
+        
+        devices.append(d)
+        
+        return devices
+    
+    
+class DeviceView:
+    
+    def showDevices(self,devices):
+        for d in devices:
+            print("-----------")
+            print("IP Address:" + d.ipAddress)
+            print("Port:" + str(d.port))
+            print("-----------")
+            
+            
+class DevicesController:
+    
+    def __init__(self):
+        devices = Device.findDevices()
+        
+        v = DeviceView()
+        v.showDevices(devices)
+    def filterDevices(self,filter):
+        devices = Device.findDevices(filter)
+        
+        
+c = DevicesController()
+        
+     
