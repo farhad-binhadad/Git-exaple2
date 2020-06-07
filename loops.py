197a687d546c4112e10afe31cf3caf2434fdff22
tonijechukwudi <tonijechukwudi@gmail.com>
2020-06-07 02:30:39 +0100

    First commit for branch new2

--- loops.py
+++ loops.py
@@ -3,3 +3,11 @@ assert a == 4
 print ("My name is Chukwudi Theodore Onije")
 
 line for new branch_new
+
+
+added branch new2 for try:
+    pass
+except expression as identifier:
+    pass
+else:
+    pass
